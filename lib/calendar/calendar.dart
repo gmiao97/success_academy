b891e28415e1973af556719900a6af73828a2e95
George Miao <gmiao97@gmail.com>
2023-06-30 09:08:02 -0400

    Shorten lesson query window until pagination is implemented.

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -73,10 +73,10 @@ class _CalendarState extends State<Calendar> {
     final singleEvents = (await event_service.listEvents(
             location: location,
             timeMin: _currentDay
-                .subtract(const Duration(days: 30))
+                .subtract(const Duration(days: 15))
                 .toIso8601String(),
             timeMax:
-                _currentDay.add(const Duration(days: 90)).toIso8601String(),
+                _currentDay.add(const Duration(days: 60)).toIso8601String(),
             singleEvents: true))
         .where((event) {
       if (!_selectedEventTypes.contains(event.eventType)) {
@@ -92,6 +92,8 @@ class _CalendarState extends State<Calendar> {
       }
       return true;
     }).toList();
+
+    print(singleEvents.length);
 
     setState(() {
       _events.clear();
