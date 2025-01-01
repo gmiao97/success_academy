c0c84a712aba1af7319f3ab23383f1cf73730375
George Miao <gmiao97@gmail.com>
2024-12-31 23:40:43 -0500

    Tweak referral type values.

--- lib/helpers/subscription.dart
+++ lib/helpers/subscription.dart
@@ -1,4 +1,4 @@
 const String freeSignUpFeeCode = 'TRYFREE';
 
-const String referralType20 = 'TWENTY';
-const String referralTypeFree = 'FREE';
+const String referralType20 = 'twenty';
+const String referralTypeFree = 'free';
